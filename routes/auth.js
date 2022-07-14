74c5e45139ad6f829dbb8a0c9936deae2901265b
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-14 17:17:10 +0530

    passsword is encrypted

--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,30 @@
 const router = require('express').Router();
 const User = require('../model/User'); 
+const { registerValidation,loginValidation } = require('./validation');
 
-
-
-
-const Schema = Joi.object({
-    name:Joi.string().min(4).required(),
-    email:Joi.string().min(4).required().email(),
-    password:Joi.string().min(5).required()
-});
+const bcrypt = require('bcryptjs')
 
 
 router.post('/register',async(req,res)=>{
 
 //validate the date before we make a user
-  
-
- const {error} = Schema.validate(req.body);
+  const {error}= registerValidation(req.body);
  if(error) return res.status(400).send(error .details [0].  message)
 
+//checking if user i already in the database
+ const  emailExist =  await  User.findOne({email:req.body.email }) ;
+ if(emailExist) return res.status(400).send('Email alredy Exists')
 
+ //Hash the passwords
+    
+   const salt  = await bcrypt.genSalt(10);
+   const hashedPassword = await bcrypt.hash(req.body.password,salt)
 
-
+//Create a new user
   const user =new User({
     name:req.body.name,
     email:req.body.email,
-    password:req.body.password,
+    password:hashedPassword ,
   });
   try{ 
      const savedUser = await user.save(); 
@@ -35,8 +34,6 @@ router.post('/register',async(req,res)=>{
   }
 
 });
-
-
 router.post('/login',(req,res)=>{
     res.send('login')
 })
--- routes/validation.js
+++ routes/validation.js
@@ -1,10 +1,27 @@
 //Validation
 const Joi = require('@hapi/joi');
+const { Schema } = require('mongoose');
+const { schema } = require('../model/User');
 
 
-//Register Validation
-const Schema = Joi.object({
-    name:Joi.string().min(4).required(),
-    email:Joi.string().min(4).required().email(),
-    password:Joi.string().min(5).required()
-});
+//Register Validation 
+
+const registerValidation=data=>{
+    const schema = Joi.object({
+        name:Joi.string().min(4).required(),
+        email:Joi.string().min(6).required().email(),
+        password:Joi.string().min(6).required()
+    });
+    return  schema.validate(data );
+};
+
+const loginValidation=data=>{
+    const Schema = Joi.object({
+       
+        email:Joi.string().min(6).required().email(),
+        password:Joi.string().min(6).required()
+    });
+    return Schema.validate(data );
+};
+module.exports.registerValidation=registerValidation;
+module.exports.loginValidation=loginValidation;
